8739448880ba070ed03f650b9b03469fb4471669
hschallh <hschallh@gmail.com>
2017-11-17 15:41:39 -0500

    Added other bank information to the bank view

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -134,7 +134,7 @@ module.exports.banks = function(req, res, next) {
                 if (err) {
                     next(err);
                 }
-                res.render('banks', { banks: result.rows, radius: radius, vender_location: vendor_location });
+                res.render('banks', { banks: result.rows, radius: radius, location: vendor_location });
             });
         });
     } else {
